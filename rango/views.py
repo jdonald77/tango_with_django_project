1309d3f2db6568877300c4945703849c4e2c8668
Joshua Donald <3009507D@student.gla.ac.uk>
2026-02-09 19:47:31 +0000

    Chapter 3

---
+++ rango/views.py
@@ -0,0 +1,16 @@
+# Create your views here.
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse(
+        'Rango says hey there partner!<br />'
+        '<a href="/rango/about/">About</a>'
+    )
+
+def about(request):
+    return HttpResponse(
+        'Rango says here is the about page.<br />'
+        '<a href="/rango/">Back to Index</a>'
+    )
