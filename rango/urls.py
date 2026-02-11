f0cc65b3be101ab53d526c8d380076e351f2d485
Joshua Donald <3009507D@student.gla.ac.uk>
2026-02-11 16:11:01 +0000

    Chapter 4

--- rango/urls.py
+++ rango/urls.py
@@ -1,9 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
-from rango import views
+from . import views
 
 app_name = 'rango'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('message/', views.message, name='message'),
     path('about/', views.about, name='about'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- rango/views.py
+++ rango/views.py
@@ -4,13 +4,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse(
-        'Rango says hey there partner!<br />'
-        '<a href="/rango/about/">About</a>'
-    )
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse(
-        'Rango says here is the about page.<br />'
-        '<a href="/rango/">Back to Index</a>'
-    )
+    context_dict = {'boldmessage': 'This tutorial has been put together by Joshua Donald'}
+    return render(request, 'rango/about.html', context=context_dict)
+
+def message(request):
+    context_dict = {'message': 'This is my message'}
+    return render(request, 'rango/message.html', context=context_dict)
